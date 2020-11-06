5a7d6f633b053b7d618514aefc2371528951a54e
Johan Schött <johan.schott@orexplore.com>
2020-11-06 15:39:15 +0100

    Activate environment in bash-script

--- use_model_tf.sh
+++ use_model_tf.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Change to script folder.
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Activate virtual environment and append to PYTHONPATH.
+source env.sh
+
 # ----Kungsholmen-----
 # Model 1
 #python -m apartment_prices.use_model_tf kungsholmen1
