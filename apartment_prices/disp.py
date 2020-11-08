983b1f31b4f3f0a175eb4f2787ea98e199215d9d
Johan Schott <johan.schott@gmail.com>
2020-11-08 12:24:47 +0100

    Print price per square meter for each apartment. And print information both for sold-date and current time.

--- apartment_prices/disp.py
+++ apartment_prices/disp.py
@@ -2,6 +2,7 @@
 
 import time
 from datetime import datetime
+import numpy as np
 
 
 def apartment_into(features, apartment, model=None):
@@ -12,9 +13,20 @@ def apartment_into(features, apartment, model=None):
         else:
             print(feature + ': {:.2f}'.format(value))
     if model != None:
+
+        area_index = np.where(features == 'livingArea')[0][0]
+        area = apartment[area_index]
         t0 = time.time()
-        print('Predicted apartment price: {:.1f} Msek.'.format(model.predict(apartment) / 10**6))
-        print("It took {:.1f} ms to predict the prize.\n".format(1000 * (time.time() - t0)))
+        price = model.predict(apartment)
+        print("It took {:.1f} ms to predict the prize.".format(1000 * (time.time() - t0)))
+        print('Predicted apartment price at sold date is {:.1f} Msek. Corresponds to {:.1f} ksek/m2.'.format(
+            price / 10**6, price / (area * 10**3)))
+        time_index = np.where(features == 'soldDate')[0][0]
+        apartment_current_time = apartment.copy()
+        apartment_current_time[time_index] = datetime.now().timestamp()
+        price = model.predict(apartment_current_time)
+        print('Predicted apartment price now is {:.1f} Msek. Corresponds to {:.1f} ksek/m2.\n'.format(
+            price / 10**6, price / (area * 10**3)))
+
     else:
         print('')
-
