38c12ed942ffc430c51fa57f80c18fb25b7cf025
Johan Schott <johan.schott@gmail.com>
2022-03-06 12:43:40 +0100

    Cartopy on Mac OSX is not working so well. Add some ugly hacks.

--- install.sh
+++ install.sh
@@ -27,6 +27,20 @@ pip install --disable-pip-version-check -q -U pip==22.0.3
 pip install --disable-pip-version-check -q pip-tools==6.5.1
 rm -f requirements.txt
 if [ "$(uname)" == "Darwin" ]; then
+    # Follow instructions from https://scitools.org.uk/cartopy/docs/latest/installing.html
+    pip install --upgrade pyshp
+    pip install shapely --no-binary shapely
+    brew install pkg-config
+    export PKG_CONFIG_PATH=/usr/local/bin/pkgconfig
+    # On my laptop, Cartopy wants to use libproj.22.dylib (which does not exist).
+    # I think Cartopy should use libproj.dylib.
+    # As a work-around, I created a symbolic link.
+    # This issue is discussed in:
+    # https://github.com/SciTools/cartopy/issues/823
+    # https://stackoverflow.com/questions/56466395/libproj-not-loaded-while-installing-sumo-on-macos#
+    # This is the command I ran to create the symbolic link to make Cartopy work:
+    # ln -s  /usr/local/opt/proj/lib/libproj.dylib /usr/local/opt/proj/lib/libproj.22.dylib
+
     pip-compile -q requirements-OSX.in --output-file requirements.txt
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     pip install --disable-pip-version-check -q $(cat requirements.in | grep numpy)
